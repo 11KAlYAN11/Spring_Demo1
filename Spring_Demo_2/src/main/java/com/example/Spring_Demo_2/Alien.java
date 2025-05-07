a05cd3603c1158e4cde566aace1469d873193805
kalyan <programmer143143@gmail.com>
2025-05-07 17:10:09 +0000

    laptop DI

--- Spring_Demo_2/src/main/java/com/example/Spring_Demo_2/Alien.java
+++ Spring_Demo_2/src/main/java/com/example/Spring_Demo_2/Alien.java
@@ -1,10 +1,17 @@
 package com.example.Spring_Demo_2;
 
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Component
 public class Alien {
-    public void code() {
+   /*  public void code() {
         System.out.println("I'm Coding...");
+    } */
+
+    @Autowired
+    Laptop lap;
+    public void code() {
+        lap.compile();
     }
 }
---
+++ Spring_Demo_2/src/main/java/com/example/Spring_Demo_2/Laptop.java
@@ -0,0 +1,10 @@
+package com.example.Spring_Demo_2;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+    public void compile() {
+        System.out.println("Compileing......");
+    }
+}
